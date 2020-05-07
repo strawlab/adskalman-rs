fc1df40b88c5150a814377e4a55a000f2152b868
Andrew Straw <strawman@astraw.com>
2020-05-07 13:43:43 +0200

    move pretty_print for nalgebra into this crate

--- src/lib.rs
+++ src/lib.rs
@@ -9,8 +9,6 @@
 
 #[cfg(feature="std")]
 use log::trace;
-#[cfg(feature="std")]
-use pretty_print_nalgebra::pretty_print;
 #[cfg(debug_assertions)]
 use approx::assert_relative_eq;
 use nalgebra as na;
@@ -41,6 +39,27 @@ macro_rules! debug_assert_symmetric {
         }
     }
 }
+
+/// convert an nalgebra array to a String
+#[cfg(feature="std")]
+macro_rules! pretty_print {
+    ($arr:expr) => {{
+        let indent = 4;
+        let prefix = String::from_utf8(vec![b' '; indent]).unwrap();
+        let mut result_els = vec!["".to_string()];
+        for i in 0..$arr.nrows() {
+            let mut row_els = vec![];
+            for j in 0..$arr.ncols() {
+                row_els.push(format!("{:12.3}", $arr[(i,j)]));
+            }
+            let row_str = row_els.into_iter().collect::<Vec<_>>().join(" ");
+            let row_str = format!("{}{}", prefix, row_str);
+            result_els.push( row_str );
+        }
+        result_els.into_iter().collect::<Vec<_>>().join("\n")
+    }}
+}
+
 
 mod error;
 pub use error::{KalmanError, ErrorKind};
